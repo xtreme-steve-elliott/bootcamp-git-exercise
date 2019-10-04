e4c609a0cd10734c0836e3720e383e16ec9fe73c
Fredrich Ombico <fombico@pivotal.io>
2019-10-03 21:11:48 -0400

    Step 6A

---
+++ src/test/java/ca/vmwbootcamp/gitexercise/Exercise6Tests.java
@@ -0,0 +1,15 @@
+package ca.vmwbootcamp.gitexercise;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Exercise 6 Tests")
+class Exercise6Tests {
+    @Test
+    @DisplayName("Exercise 6")
+    void exercise6() {
+        assertThat(7).isEqualTo(7);
+    }
+}
